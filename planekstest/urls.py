7de8c414716a09062a504f112e18492058913246
unknown <slavko.dem@gmail.com>
2020-12-09 12:55:20 +0200

    Modified settings

--- planekstest/urls.py
+++ planekstest/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from schemas.views import Schemas
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('', Schemas.as_view()),
@@ -8,3 +10,6 @@ urlpatterns = [
     path('authentication/', include('authentication.urls')),
     path('schemas/', include('schemas.urls')),
 ]
+urlpatterns += static('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
