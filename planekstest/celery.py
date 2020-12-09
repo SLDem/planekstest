581b3dd6ab663699522fe353b69cf42e46fc1e86
unknown <slavko.dem@gmail.com>
2020-12-09 15:42:38 +0200

    Modified settings

--- planekstest/celery.py
+++ planekstest/celery.py
@@ -12,7 +12,7 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 app.conf.update(BROKER_URL=os.environ.get('REDIS_URL'),
-                CELERY_RESULT_BACKEND=os.environ.get('REDIS_URL'))
+                CELERY_RESULT_BACKEND='django-db')
 
 
 @app.task(bind=True)
