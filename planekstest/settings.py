1acef38945877c3ff0b6000a09bc778040cd6784
unknown <slavko.dem@gmail.com>
2020-12-09 12:19:18 +0200

    Modified settings

--- planekstest/settings.py
+++ planekstest/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = 'schemas'
 LOGOUT_REDIRECT_URL = 'login'
