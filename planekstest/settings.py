e5c7a508327603f0da7633cf264cca0725bbdeac
unknown <slavko.dem@gmail.com>
2020-12-10 11:57:44 +0200

    Changed settings

--- planekstest/settings.py
+++ planekstest/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_celery_results',
     'authentication',
     'accounts',
     'schemas',
@@ -134,14 +135,15 @@ STATICFILES_DIRS = (
 LOGIN_REDIRECT_URL = 'schemas'
 LOGOUT_REDIRECT_URL = 'login'
 
-CELERY_BROKER_URL = 'redis://:p98ee57b101ee894443738f814985b4b093adbca43d64042e65cfca069be4555c@ec2-50-16-128-84.compute-1.amazonaws.com:12099'
-CELERY_RESULT_BACKEND = 'redis://:p98ee57b101ee894443738f814985b4b093adbca43d64042e65cfca069be4555c@ec2-50-16-128-84.compute-1.amazonaws.com:12099'
+#  CELERY_BROKER_URL = 'redis://:p98ee57b101ee894443738f814985b4b093adbca43d64042e65cfca069be4555c@ec2-50-16-128-84.compute-1.amazonaws.com:12099'
+CELERY_RESULT_BACKEND = 'django-db'
+
+CELERY_CACHE_BACKEND = "db+sqlite:///celery.sqlite"
+CELERY_BROKER_BACKEND = "db+sqlite:///celery.sqlite"
 
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_ACCEPT_CONTENT = ['json']
 
-CELERY_TRACK_STARTED = True
-CELERY_IGNORE_RESULT = False
 
 django_heroku.settings(locals())
