ac195ca2e68dde39997786ccedb1f962b5af256d
unknown <slavko.dem@gmail.com>
2020-12-10 15:27:36 +0200

    Changed db to postgres

--- planekstest/settings.py
+++ planekstest/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_celery_results',
     'authentication',
     'accounts',
     'schemas',
@@ -81,10 +80,15 @@ WSGI_APPLICATION = 'planekstest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dfgie2ogqr9pjq',
+        'USER': 'ogangyhnjgurgn',
+        'PASSWORD': 'e70ecabd80acbc12e6c8c927afda66dfd6e13145036b52d43f53654a3fddca25',
+        'HOST': 'ec2-54-160-73-231.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
